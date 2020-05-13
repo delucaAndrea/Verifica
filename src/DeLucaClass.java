8db790efaddc6a43b85b3192a9a19ea864311b8f
Andrea <Andrea De Luca>
2020-05-13 11:14:07 +0200

    versione 0

---
+++ src/DeLucaClass.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @author Andrea<h3> De Luca</h3> <u> 4Ai </u> <em>13/5/2020</em>
+ *
+ *
+ */
+public class DeLucaClass {
+	private int b,e;
+/**
+ * 
+ * @param b
+ * @param e
+ * @return
+ */
+	public float DeLucaMet(int b, int e) {
+		int val1=1;
+		for (int i=1;i<e;i++) {//nel for bisogna inserire "e" al posto di "b" 
+								//inoltre bisogna inserire i<e e non i<=e
+			val1=val1*b;
+		}
+		return val1;
+	}
+/**
+ * 
+ * @param args
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
